053194d0b2b8800e754f7dc97c35622641a9578f
Alan Boaventura <alanboaventurab@gmail.com>
2018-11-28 21:15:00 -0200

    Criar comparador por título

---
+++ src/ComparadorLivroPorTitulo.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class ComparadorLivroPorTitulo implements Comparator<Livro> {
+
+    @Override
+    public int compare(Livro livro1, Livro livro2) {
+        return livro1.getTitulo().compareTo(livro2.getTitulo());
+    }
+}
